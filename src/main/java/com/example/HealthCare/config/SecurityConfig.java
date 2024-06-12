8c710ddb6bb8ad0ba32ca467a7d5cd6052b648cf
Javohir <@javohirjahonov>
2024-06-12 17:14:45 +0500

    Update Configuration

--- src/main/java/com/example/HealthCare/config/SecurityConfig.java
+++ src/main/java/com/example/HealthCare/config/SecurityConfig.java
@@ -1,72 +1,70 @@
-    package com.example.HealthCare.config;
+package com.example.HealthCare.config;
 
-    import com.example.HealthCare.filter.JwtFilterToken;
-    import com.fasterxml.jackson.databind.ObjectMapper;
-    import lombok.RequiredArgsConstructor;
-    import org.springframework.context.annotation.Bean;
-    import org.springframework.context.annotation.Configuration;
-    import org.springframework.security.authentication.AuthenticationProvider;
-    import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-    import org.springframework.security.config.http.SessionCreationPolicy;
-    import org.springframework.security.web.AuthenticationEntryPoint;
-    import org.springframework.security.web.SecurityFilterChain;
-    import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-    import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-    import org.springframework.web.cors.CorsConfiguration;
-    import org.springframework.web.cors.CorsConfigurationSource;
-    import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import com.example.HealthCare.filter.JwtFilterToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-    import java.util.Arrays;
-    import java.util.List;
+import java.util.Arrays;
+import java.util.List;
 
-    @Configuration
-    @EnableWebSecurity
-    @EnableMethodSecurity
-    @RequiredArgsConstructor
-    public class SecurityConfig {
-        private final JwtFilterToken authFilter;
-        private final AuthenticationProvider authenticationProvider;
-        private final ObjectMapper objectMapper;
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtFilterToken authFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final ObjectMapper objectMapper;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
-        private static final String[] WHITE_LIST_URL = {
-                "/swagger-ui/**", "/v3/api-docs/**", "/user/auth/**", "/text-to-text/**"
-        };
+    private static final String[] WHITE_LIST_URL = {
+            "/swagger-ui/**", "/v3/api-docs/**", "/user/auth/**", "/text-to-text/**"
+    };
 
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-            httpSecurity
-                    .cors(c -> c.configurationSource(corsConfigurationSource())) // CORS configuration first
-                    .csrf(AbstractHttpConfigurer::disable)
-                    .authorizeHttpRequests(request -> request
-                            .requestMatchers(WHITE_LIST_URL)
-                            .permitAll()
-                            .anyRequest()
-                            .authenticated()
-                    )
-                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                    .authenticationProvider(authenticationProvider)
-                    .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-                    .exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint()));
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors(c -> c.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(WHITE_LIST_URL)
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint));
 
-            return httpSecurity.build();
-        }
-
-        private AuthenticationEntryPoint authenticationEntryPoint() {
-            return new CustomAuthenticationEntryPoint(objectMapper);
-        }
-
-
-        @Bean
-        CorsConfigurationSource corsConfigurationSource() {
-            CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(List.of("https://8088-cs-74391610147-default.cs-asia-east1-vger.cloudshell.dev")); // Your Google Cloud Shell URL
-            configuration.setAllowedMethods(Arrays.asList("*"));
-            configuration.setAllowedHeaders(List.of("*"));
-            configuration.setAllowCredentials(true);
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            source.registerCorsConfiguration("/**", configuration);
-            return source;
-        }
+        return httpSecurity.build();
     }
+
+    // ... (authenticationEntryPoint method)
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://8088-cs-74391610147-default.cs-asia-east1-vger.cloudshell.dev")); // Your Google Cloud Shell URL
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
